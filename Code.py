159699559d9bbb375e391bd01b143e941897f6b2
Millibort <91936081+Millibort@users.noreply.github.com>
2023-02-14 00:33:49 -0800

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,21 @@
+import os
+import discord
+
+with open('TOKEN.txt', 'r') as f:
+    TOKEN = f.read()
+
+intents = discord.Intents.default()
+intents.messages = True
+intents.message_content = True
+
+client = discord.Client(intents = intents)
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name = 'Linux!'))
+    print(f'{client.user.name} is ready')
+
+@client.event
+async def on_message(message):
+    print(message.author.name + ' said: ' + message.author.id)
+client.run(TOKEN)
